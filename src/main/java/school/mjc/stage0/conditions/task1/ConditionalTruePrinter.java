8f5ee8172c909b6c91a17743de2771a839154056
Fanis <mrduck597@gmail.com>
2023-05-07 12:05:56 +0500

    test 1

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+
+        if (1<2) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
     }
 }
